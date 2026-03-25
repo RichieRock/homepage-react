1c4c987e4f63e22a066fa957aa8049bfc710908e
Riku Kallio <ext-riku.kallio@vaisala.com>
2026-03-25 15:39:24 +0200

    updated tech for better responsiveness

--- src/data/languages.tsx
+++ src/data/languages.tsx
@@ -1,29 +1,34 @@
 export enum LanguageLevel {
-  NATIVE = 'Native',
-  FULL = 'Full professional Proficiency',
-  GOOD = 'Good communication skills',
+  NATIVE = "Native",
+  FULL = "Full professional Proficiency",
+  GOOD = "Good communication skills",
+  BEGINNER = "Learning",
 }
 
 export interface Language {
-  name: string
-  level: LanguageLevel
+  name: string;
+  level: LanguageLevel;
 }
 
 export const languages: Language[] = [
   {
-    name: 'English',
+    name: "English",
     level: LanguageLevel.FULL,
   },
   {
-    name: 'Finnish',
+    name: "Suomi",
     level: LanguageLevel.NATIVE,
   },
   {
-    name: 'Swedish',
+    name: "Svenska",
     level: LanguageLevel.GOOD,
   },
   {
-    name: 'French',
+    name: "Français",
     level: LanguageLevel.GOOD,
   },
-]
+  {
+    name: "中文 (Chinese)",
+    level: LanguageLevel.BEGINNER,
+  },
+];
